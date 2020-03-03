815cf3f33c17258df80ef44b92e70b48e02ddf10
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-03-03 20:53:56 +0900

    refactor

--- src/createGame.ts
+++ src/createGame.ts
@@ -1,20 +1,23 @@
 import { Card } from "./Card";
-import { AlgorithToChooseCandidate, Game } from "./Types";
+import { AlgorithToChooseCandidate, Game, CursorDirection } from "./Types";
 export const createGame = (initialLife: number, algorithm0: AlgorithToChooseCandidate, algorithm1: AlgorithToChooseCandidate, cards: Card[], maxFlag = 10, maxLife = 6): Game => {
-  let game = {} as Game;
-  game.cursor = {
-    cardIndex: 0,
-    flagIndex: 0,
-    repeatIndex: 1,
+  let game = {
+    cursor: {
+      cardIndex: 0,
+      flagIndex: 0,
+      repeatIndex: 1,
+    },
+    returnAddress: null,
+    maxFlag: maxFlag,
+    maxLife: maxLife,
+    cursorDirection: "forward" as CursorDirection,
+    time: 0,
+    players: [
+      { life: initialLife, color: "red", chooseFromCandidate: algorithm0 },
+      { life: initialLife, color: "blue", chooseFromCandidate: algorithm1 },
+    ],
+    cards: cards,
+
   };
-  game.returnAddress = null;
-  game.maxFlag = maxFlag;
-  game.maxLife = maxLife;
-  game.cursorDirection = "forward";
-  game.players = [
-    { life: initialLife, color: "red", chooseFromCandidate: algorithm0 },
-    { life: initialLife, color: "blue", chooseFromCandidate: algorithm1 },
-  ];
-  game.cards = cards;
   return game;
 };
