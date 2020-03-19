ebeeff4cacf390542606fc5c898f375548d499e7
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-03-19 16:40:57 +0900

    fix bug of removing first card

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { createGame } from "./createGame";
+import { RemoveCommand } from "./card/RemoveCommand";
+import { chooseRandom } from "./player/chooseRandom";
+import { Bug } from "./card/Bug";
+import { FirstPlayer, SecondPlayer } from "./Types";
+import { gameToStr } from "./debugPrint";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("foo", () => {
+  const game = createGame({
+    initialLife: 1,
+    maxFlag: 1,
+    maxLife: 6,
+    algorithmOfFirstPlayer: chooseRandom,
+    algorithmOfSecondPlayer: chooseRandom,
+    numInitialFlag: 1,
+    cards: [RemoveCommand(), Bug()]
+  });
+  game.cards[0].flags.push(FirstPlayer);
+  game.cards[1].flags.push(SecondPlayer);
+  game.phase = "RunProgram";
+
+  const candidate = game.cards[0].getCandidate(game, FirstPlayer);
+  // console.log(
+  //   candidate.map(game => {
+  //     return [gameToStr(game), game.cursor];
+  //   })
+  // );
+  expect(candidate[1].cursor.cardIndex).not.toBe(-1);
 });
--- src/card/RemoveCommand.ts
+++ src/card/RemoveCommand.ts
@@ -9,11 +9,27 @@ const getCandidate = (game: Game, playerId: PlayerID) => {
     newCards.splice(cardIndex, 1);
     let newCursor = { ...game.cursor };
     if (cardIndex < game.cursor.cardIndex) {
-      newCursor.cardIndex--;
+      newCursor = {
+        cardIndex: game.cursor.cardIndex - 1,
+        flagIndex: game.cursor.flagIndex, // keep
+        repeatIndex: game.cursor.repeatIndex // keep
+      };
     } else if (cardIndex === game.cursor.cardIndex) {
       // remove self
       if (game.cursorDirection === "forward") {
-        newCursor.cardIndex--;
+        if (cardIndex === 0) {
+          newCursor = {
+            cardIndex: newCards.length - 1, // last card
+            flagIndex: 4, // last
+            repeatIndex: 4 // last
+          };
+        } else {
+          newCursor = {
+            cardIndex: game.cursor.cardIndex - 1, // previous card
+            flagIndex: 4, // last
+            repeatIndex: 4 // last
+          };
+        }
       } else {
         // when cursorDirection is backward, keep cardIndex
         // except for RemoveCommand is the last card
@@ -21,11 +37,16 @@ const getCandidate = (game: Game, playerId: PlayerID) => {
           newCursor = {
             cardIndex: 0,
             flagIndex: 4,
-            repeatIndex: 1
+            repeatIndex: 4
+          };
+        } else {
+          newCursor = {
+            cardIndex: game.cursor.cardIndex - 1, // previous card
+            flagIndex: 4, // last
+            repeatIndex: 4 // last
           };
         }
       }
-      newCursor.flagIndex = 4;
     }
     candidate.push({
       ...game,
